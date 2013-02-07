218c4b64ed3f7065aa341f9b0e51c04a791fa3d9
MichaelAz <michael.azarkevich@gmail.com>
2013-02-07 23:26:49 +0200

    This commit fixes issue #624
    Encapsulates the "hotfix" for #624 as a bootstrapper for testing
    purposes, "TestingBootstrapper"

---
+++ src/Nancy.Testing/TestingBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Testing
+{
+    public class TestingBootstrapper : ConfigurableBootstrapper
+    {
+        // Hotfix for issue #624
+        public TestingBootstrapper() : base(x => x.RootPathProvider(new DefaultRootPathProvider()))
+        {
+            
+        }
+    }
+}
